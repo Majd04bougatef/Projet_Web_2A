d141ccfecb85e6f8bf2582619d9997e6f3b82917
arij203 <arijtalhaoui@gmail.com>
2023-12-05 22:02:38 +0100

    2éme entité

---
+++ travail/BLOG/Controller/blogC.php
@@ -0,0 +1,112 @@
+<?php
+include_once '../config.php';
+include_once '../Model/blog.php';
+
+class BlogC
+{
+    public function listBlogs()
+    {
+        $sql = "SELECT * FROM blog WHERE isdelete=0";
+        $db = config::getConnexion();
+        try {
+            $liste = $db->query($sql);
+            return $liste;
+        } catch (Exception $e) {
+            die('Error:' . $e->getMessage());
+        }
+    }
+
+    public function deleteBlog($id)
+    {
+        $sql = "UPDATE blog SET isdelete = 1 WHERE id_b = :id";
+        $db = config::getConnexion();
+        $req = $db->prepare($sql);
+        $req->bindValue(':id', $id, PDO::PARAM_INT);
+
+        try {
+            $req->execute();
+        } catch (Exception $e) {
+            die('Error:' . $e->getMessage());
+        }
+    }
+
+    public function addBlog($blog)
+    {
+        $sql = "INSERT INTO `blog` (`id_b`, `titre_blog`, `sujet_blog`, `desc_blog`, `date_blog`, `id_user`) 
+                VALUES (NULL, :titre_blog, :sujet_blog, :desc_blog, :date_blog, 'MM12345676');";
+
+        $db = config::getConnexion();
+        try {
+            $query = $db->prepare($sql);
+            $query->execute([
+                'titre_blog' => $blog->getTitreBlog(),
+                'sujet_blog' => $blog->getSujetBlog(),
+                'desc_blog' => $blog->getDescBlog(),
+                'date_blog' => date('Y-m-d')
+            ]);
+
+            echo 'Blog added successfully.';
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+
+    public function updateBlog($blog, $id)
+    {
+        try {
+            $db = config::getConnexion();
+            $query = $db->prepare(
+                'UPDATE blog SET 
+                    titre_blog = :titre_blog, 
+                    sujet_blog = :sujet_blog, 
+                    desc_blog = :desc_blog, 
+                    date_blog = :date_blog
+                WHERE id_b = :id_b'
+            );
+            $query->execute([
+                'id_b' => $id,
+                'titre_blog' => $blog->getTitreBlog(),
+                'sujet_blog' => $blog->getSujetBlog(),
+                'desc_blog' => $blog->getDescBlog(),
+                'date_blog' => $blog->getDateBlog()->format('Y-m-d')
+            ]);
+            
+            echo $query->rowCount() . " records UPDATED successfully <br>";
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+
+    public function showBlog($id)
+    {
+        $sql = "SELECT * FROM blog WHERE id_b = :id AND isdelete = 0";
+        $db = config::getConnexion();
+        try {
+            $query = $db->prepare($sql);
+            $query->bindValue(':id', $id, PDO::PARAM_INT);
+            $query->execute();
+
+            $blog = $query->fetch();
+            return $blog;
+        } catch (Exception $e) {
+            die('Error: ' . $e->getMessage());
+        }
+    }
+    public function getBlogDetails($blogId) {
+        $sql = "SELECT * FROM blog WHERE id_b = :id AND isdelete = 0";
+        $db = config::getConnexion();
+        
+        try {
+            $query = $db->prepare($sql);
+            $query->bindValue(':id', $blogId, PDO::PARAM_INT);
+            $query->execute();
+    
+            $blog = $query->fetch();
+            return $blog;
+        } catch (Exception $e) {
+            // Handle the exception, e.g., log it or show an error message
+            return false;
+        }
+    }
+}
+?>
---
+++ travail/BLOG/Controller/liste_commentaires.php
@@ -0,0 +1,125 @@
+<?php
+include '../Controller/commentC.php';
+$commentaireC = new commentC();
+$listCommentaires = $commentaireC->listCommentaires();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commentaire Display</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+    
+        h2{
+            text-align: left;
+            color: #333;
+        }
+
+        a {
+            text-decoration: none;
+            color: #3498db;
+            font-weight: bold;
+        }
+
+        table {
+            border-collapse: collapse;
+            margin: auto;
+            margin-top: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            width: 100%;
+            
+        }
+
+        th, td {
+            padding: 15px;
+            text-align: center;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #3498db;
+            color: #fff;
+        }
+
+        td {
+            background-color: #fff;
+        }
+
+        form {
+            display: inline-block;
+        }
+
+        input[type="submit"] {
+            background-color: #27ae60;
+            color: #fff;
+            border: none;
+            padding: 8px 12px;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #219d54;
+        }
+
+        .delete-link {
+            color: #e74c3c;
+            font-weight: bold;
+        }
+
+        .delete-link:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+   
+    <!--<h2><a href="add_commentaire.php">Ajouter un commentaire</a></h2>-->
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Date du commentaire</th>
+            <th>Description du commentaire</th>
+            <th>Mise à jour</th>
+            <th>Supprimer</th>
+        </tr>
+        <?php
+        foreach ($listCommentaires as $commentaire) {
+        ?>
+            <tr>
+                <td><?= $commentaire['id_com']; ?></td>
+                <td><?= $commentaire['date_commentaire']; ?></td>
+                <td><?= $commentaire['desc_commentaire']; ?></td>
+                <td>
+                    <form method="POST" action="update_commentaire.php">
+                        <input type="submit" name="update" value="Mise à jour">
+                        <input type="hidden" value=<?= $commentaire['id_com']; ?> name="id_com">
+                    </form>
+                </td>
+                <td>
+                    <a class="delete-link" href="delete_commentaire.php?id_com=<?= $commentaire['id_com']; ?>">Supprimer</a>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+</body>
+
+</html>
